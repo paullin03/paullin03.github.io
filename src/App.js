e3b0f187a725f8f6602463b29465de70799ef0e8
Paul Lin <linpaul0328@gmail.com>
2019-04-19 23:15:14 -0700

    Added Tabs and fixed colour scheme

--- src/App.js
+++ src/App.js
@@ -1,58 +1,90 @@
 import React, { Component } from 'react';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Tab from 'react-bootstrap/Tab';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar bg="dark" expand="lg" variant="dark">
-          <Navbar.Brand>
+      <div style={{background: "#222222"}}>
+        <Navbar bg="dark" expand="lg" variant="dark" id="NavigationBar">
+          <Navbar.Brand href="#AboutMe">
             Paul Lin
           </Navbar.Brand>
           <NavDropdown title="Projects">
-            <NavDropdown.Item href="#softwareHeading">
+            <NavDropdown.Item href="#Software">
               Software
             </NavDropdown.Item>
-            <NavDropdown.Item href="#hardwareHeading">
+            <NavDropdown.Item href="#Hardware">
               Hardware
             </NavDropdown.Item>
           </NavDropdown>
-        </Navbar>
-        <h1 id="aboutMeHeading">About Me</h1>
-        <h1 id="softwareHeading">Software</h1>
+        </Navbar>          
+        <h1 id="AboutMe">About Me</h1>
         <Container>
-          <Card style={{width: '18rem'}} bg="light">
-            <Card.Body>
-              <Card.Title>Android Task Organizer</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-            </Card.Body>
-          </Card>
         </Container>
-        <h1 id="hardwareHeading">Hardware</h1>
         <Container>
-          <Card style={{width: '18rem'}} bg="light">
-            <Card.Body>
-              <Card.Title>Simple RISC Machine</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-            </Card.Body>
-          </Card>
+        </Container>
+
+        <h1 id="Software">Software</h1>
+        <div style={{padding:"1.5rem"}}>
+          <Tab.Container id="SoftwareProjects" defaultActiveKey="taskOrganizer">
+            <Row>
+              <Col md="auto">
+                <Nav variant="pills" className="flex-column">
+                  <Nav.Item>
+                    <Nav.Link eventKey="taskOrganizer">Task Organizer</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="apps">Apps</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+              <Col>
+                <Tab.Content>
+                  <Tab.Pane eventKey="taskOrganizer">
+                    {this.generateCard("Task Organizer", "Android App for Organizing Tasks", "Lorem Ipsum", "https://github.com/paullin03/TaskOrganizer")}
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="apps">
+                    {this.generateCard("apps", "apps", "Lorem Ipsum", "#AboutMe")}
+                  </Tab.Pane>
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
+        </div>
+
+        <h1 id="Hardware">Hardware</h1>
+
+        <Container>
+          <Row>
+            {this.generateCard("Simple RISC Machine", "Turing-Complete Computer", "Lorem Ipsum", "#AboutMe")}
+          </Row>
         </Container>
       </div>
       
+    );
+  }
+
+
+  generateCard(title, subtitle, text, href) {
+    return(
+      <Card bg="light">
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{subtitle}</Card.Subtitle>
+          <Card.Text>
+            {text}
+          </Card.Text>
+          <Card.Link href={href} target="_blank">GitHub Link</Card.Link>
+        </Card.Body>
+      </Card>
     );
   }
 }
